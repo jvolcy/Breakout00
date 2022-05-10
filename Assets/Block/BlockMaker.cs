d59c9821a459a7329d8f6e8fd6eb1b8440e70427
jvolcy <jerry.volcy@gmail.com>
2022-05-09 20:14:10 -0400

    block color gradient added

--- Assets/Block/BlockMaker.cs
+++ Assets/Block/BlockMaker.cs
@@ -10,6 +10,8 @@ public class BlockMaker : MonoBehaviour
     public float ColSpacing = 1f;
     public float RowSpacing = 0.75f;
     public Vector2 CenterBlockPos = Vector2.zero;
+    public Color32 TopRowColor;
+    public Color32 BottomRowColor;
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +27,24 @@ public class BlockMaker : MonoBehaviour
 
     void MakeBlocks()
     {
+        Color32 color = new Color32();
+
         GameObject gameObject;
 
         for (int j = -NumOddRows / 2; j <= NumOddRows / 2; j++)
         {
+            color.r = (byte)(BottomRowColor.r + (j - (int)(-NumOddRows / 2)) * (int)(TopRowColor.r - BottomRowColor.r) / NumOddRows);
+            color.g = (byte)(BottomRowColor.g + (j - (int)(-NumOddRows / 2)) * (int)(TopRowColor.g - BottomRowColor.g) / NumOddRows);
+            color.b = (byte)(BottomRowColor.b + (j - (int)(-NumOddRows / 2)) * (int)(TopRowColor.b - BottomRowColor.b) / NumOddRows);
+            color.a = (byte)(BottomRowColor.a + (j - (int)(-NumOddRows / 2)) * (int)(TopRowColor.a - BottomRowColor.a) / NumOddRows);
+
             for (int i = -NumOddCols / 2; i <= NumOddCols / 2; i++)
             {
                 float XPos = CenterBlockPos.x + i * ColSpacing;
                 float YPos = CenterBlockPos.y + j * RowSpacing;
                 gameObject = Instantiate(BlockPrefab);
                 gameObject.transform.position = new Vector2(XPos, YPos);
+                gameObject.GetComponent<SpriteRenderer>().color = color;
             }
         }
     }
